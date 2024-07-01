f24575b03b65f091203ae5f6b2bc28da86c17f40
vladislav-chihun <vladikkosmo@gmail.com>
2024-07-01 21:48:13 +0200

    create generics

---
+++ ts-hw-1/src/generics/5.ts
@@ -0,0 +1,14 @@
+export enum UserRole {
+  admin = "admin",
+  editor = "editor",
+  guest = "guest",
+}
+
+type UserRoleDescription = Record<UserRole, string>;
+
+// Замініть наступний код на версію за допомогою Record
+const RoleDescription: UserRoleDescription = {
+  [UserRole.admin]: "Admin User",
+  [UserRole.editor]: "Editor User",
+  [UserRole.guest]: "Guest User",
+};
